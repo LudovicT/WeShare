c1e9392d82f2d9f325212a1efc927fed58811a09
Vincent <ricard@intechinfo.fr>
2012-04-26 12:29:36 +0200

    Ajout de dernière version de createEvent() dans mainModels.php
    Signed-off-by: Vincent <ricard@intechinfo.fr>

--- application/models/mainModels.php
+++ application/models/mainModels.php
@@ -781,5 +781,40 @@ function	getEvents($IdUser)
 	return ($Events);
 }
 
+/*
+La fonction createEvent permet à l'utilisateur de créer un événement.
+$error
 
+$error (S): int
+1	:	erreur requête invalide/problème avec la BDD;
+0	:	OK
+
+Auteur : Vincent Ricard
+*/
+
+function	createEvent($IdUser, $DateOfEvent, $Adress, $City/*, $PollEnding*/)
+{
+	$error = 0;
+	var_dump($IdUser);
+	var_dump($DateOfEvent);
+	var_dump($Adress);
+	var_dump($City);
+	$query = sprintf("INSERT INTO Events 
+					  (DateOfEvent, Adress, City, CreationDate, IdOrganizer) 
+					  VALUES ('%s', '%s', '%s', '%d')",
+					  $DateOfEvent,
+					  $Adress,
+					  $City,
+					  date("y-m-d"),
+					  $IdUser);
+					  /*$PollEnding,*/
+					  $IdOrganizer);
+	
+	$result = mysql_query($query, dbConnect());
+	if (!isset($result))
+	 {
+		$error = 1;
+	 }
+return ($error);
+}
 ?>
