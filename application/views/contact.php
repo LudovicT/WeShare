c0d5be2de573b6643b981bcb241903358923aa4a
Alexandre ARNAL <aarnal@intechinfo.fr>
2012-05-02 09:43:55 +0200

    Signed-off-by: Alexandre ARNAL <aarnal@intechinfo.fr>

---
+++ application/views/contact.php
@@ -0,0 +1,27 @@
+<div class="col-2">
+
+			<h3>Contactez nous !</h3>
+			
+			<div class="indent2">
+			
+				<form id="contacts-form" action="">
+				
+					<fieldset>
+					
+					<div class="field"><label>Votre Nom:</label><input type="text" value=""/></div>
+					<br />
+					<div class="field"><label>Votre Prenom:</label><input type="text" value=""/></div>
+					<br />
+					<div class="field"><label>Votre Email:</label><input type="text" value=""/></div>
+					<br />
+					<div class="field"><label>Votre Message:</label><textarea cols="1" rows="1"></textarea></div>
+					
+					<div class="wrapper"><a href="#" onclick="document.getElementById('contacts-form').submit()"><em><b>Envoyez votre message !</b></em></a></div>
+					
+					</fieldset>
+					
+				</form>
+				
+			</div>
+			
+</div>
