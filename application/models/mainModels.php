3b8eafaad78b49fd28a2024a5bb8874eda44572f
Alexandre ARNAL <aarnal@intechinfo.fr>
2012-05-02 11:55:15 +0200

    Fonction editMovie. Signed-off-by: Alexandre ARNAL <aarnal@intechinfo.fr>

--- application/models/mainModels.php
+++ application/models/mainModels.php
@@ -756,9 +756,8 @@ return ($error);
 }
 
 /*
-La fonction getEvents permet de récupérer le(s) événement(s) concernant
-un utilisateur, qu'il en soit le créateur ou qu'il y participe.
-
+La fonction getEvents permet de récupérer l'ensemble du ou des événement(s) 
+créé(s) par l'utilisateur.
 $Events
 
 $Events (S): int
@@ -957,5 +956,101 @@ function deleteMovie($MovieId)
 	{
 		return 1;
 	}
+}
+
+/* 
+La fonction editMovie sert à entrer le(s) modification(s) des films dans la base de donnée.
+$IdMovie,
+$name, 
+$synopsis, 
+$DateOfRelease, 
+$Poster,
+ 
+$error (S): int 
+				0	: OK 
+				1	: trop long ;
+				2	: erreur requête invalide/problème avec la BDD;
+				3	: erreur format image avatar invalide ;
+				Auteur: ARNAL Alexandre
+*/
+
+function editMovie($IdMovie, $name, $synopsis, $DateOfRelease, $Poster)
+{
+	$error = 0;
+	if (!empty($name))
+	{
+		if (strlen($FirstName) < 55)
+		{
+			$query = sprintf("UPDATE Movies SET name = '%s' 
+							 WHERE IdMovie = '%d'",
+							 $name, $IdMovie);
+			$result = mysql_query($query, dbConnect());
+			if (!isset($result))
+			{
+				$error = 2;
+			}
+		}
+		else
+		{
+			$error = 1;
+		}
+	}
+	if (isset($synopsis))
+	{
+		if ($strlen($synopsis) < 2500 )
+		{
+			$query = sprintf("UPDATE Movies SET synopsis = '%s' 
+							WHERE IdMovie = '%d'",
+							$synopsis, $IdMovie);
+			$result = mysql_query($query, dbConnect());
+			if (!isset($result))
+			{
+				$error = 2;
+			}
+		}
+		else
+		{
+			$error = 1;
+		}
+	}
+	if (!empty($DateOfRelease))
+	{
+		if ($DateOfRelease > 2500 || $DateOfRelease < 1700)
+		{
+			$query = sprintf("UPDATE Movies SET DateOfRelease = '%s' 
+							 WHERE IdMovie = '%d'",
+							$DateOfRelease, $IdMovie);
+			$result = mysql_query($query, dbConnect());
+			if (!isset($result))
+			{
+				$error = 2;
+			}
+		}
+		else
+		{
+			$error = 1;
+		}
+	}
+	if (!empty($Poster))
+	{
+		if (stristr($Poster, ".jpg") || stristr($Poster, ".jpeg") 
+		 || stristr($Poster, ".gif") || stristr($Poster, ".png")
+		 || stristr($Poster, ".bmp"))
+		{
+			$query = sprintf("UPDATE Movies SET Poster = '%s' 
+							 WHERE IdMovie = '%d'",
+							 $Poster, $IdMovie);
+			$result = mysql_query($query, dbConnect());
+			if (!isset($result))
+			{
+				$error = 2;
+			}
+		}
+		else
+		{
+			$error = 4;
+		}
+	}
+return ($error);
 }
 ?>
