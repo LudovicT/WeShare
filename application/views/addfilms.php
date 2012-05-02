fed4d2dd8233794b29eed7bc96a42688d48c4386
Alexandre ARNAL <aarnal@intechinfo.fr>
2012-05-02 10:19:27 +0200

    jaquette Signed-off-by: Alexandre ARNAL <aarnal@intechinfo.fr>

--- application/views/addfilms.php
+++ application/views/addfilms.php
@@ -8,6 +8,7 @@
 				<span style="text-align:left;"><label>Synopsis :</label><br/><textarea id="Synopsis" name="synopsis" style="width:480px;" rows="15"></textarea></span><br /><br />
 				<span  style="text-align:left;"><label>Durée :</label><input type="text" value=""  id="duree" name="duree" style="width:23px;" size="1" /> min</span><br /><br />
 				<span  style="text-align:left;"><label>Note :</label><input type="text" value=""  id="note" style="width:18px;" size="1"  name="note" /> / 10</span><br /><br />
+				<span><label>Jaquette du film :</label><input type="text" value="" id="jaquette" name="jaquette" /></span><br /><br /><br />
 				<input type="hidden" id="affiche" name="affiche" /></span>
 				<div class="button_submit"><input type="submit" value="Ajouter le film" /></div>
 			</form>
