fa0209ab1afc6327b3db4315aa9208403a49b39e
Vincent <ricard@intechinfo.fr>
2012-05-15 13:36:03 +0200

    avancement implémentation des événements (85%) Signed-off-by: Vincent <ricard@intechinfo.fr>

--- application/views/removeEvent.php
+++ application/views/removeEvent.php
@@ -4,7 +4,26 @@
 <table border='1'>
 	<th> N° de l'événement </th><th> Date de l'événement </th><th> Adresse </th>
 	<th> Ville </th><th> Date de création </th>
-<?php
-var_dump($IdEvent);
-?>
+	<tr>
+		<td>
+<?php echo $event['IdEvent'] ?>
+		</td>
+		<td>
+<?php echo($event['DateOfEvent']); ?>	
+		</td>
+		<td>
+<?php echo($event['Address']); ?>	
+		</td>
+		<td>
+<?php echo($event['City']); ?>	
+		</td>
+		<td>
+<?php echo($event['CreationDate']); ?>	
+		</td>
+	</tr>
 </table>
+<form id="edit-profile-form" method="post" action="/WeShare/Evenements/">
+<input type="hidden" value=<?php echo $event['IdEvent'] ?>>
+<input type="submit" value="Supprimer cet événement">
+</form>
+<!--<a href='/WeShare/Evenements/Supprimer/<?php echo $event['IdEvent'] ?>/' > Supprimer cet événement</a>-->
