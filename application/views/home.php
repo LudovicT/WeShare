30e79006fcb5881f9e42e60e1d5d33c1bf64d1b7
Alexandre ARNAL <aarnal@intechinfo.fr>
2012-06-14 23:29:21 +0200

    Signed-off-by: Alexandre ARNAL <aarnal@intechinfo.fr>

--- application/views/home.php
+++ application/views/home.php
@@ -2,11 +2,11 @@
 	<br /><br /><br /><br /><br /><br />
 	<div id = "home">
 		<a href="/WeShare/Connexion/"><h1>CONNEXION</h1></a><br />
-	</div>
+	</div></br></br></br></br></br>
 
 	
 	
-	<div id = "home">
+	<div id = "home1">
 		<a href="/WeShare/Inscription/"><h1>INSCRIPTION</h1></a>
 	</div>
 	<!--End of home page-->
