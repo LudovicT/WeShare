0ad03bccffd380e372c21229e20d63addd54d99e
Alexandre ARNAL <aarnal@intechinfo.fr>
2012-06-15 09:40:38 +0200

    Signed-off-by: Alexandre ARNAL <aarnal@intechinfo.fr>

--- application/views/editProfil.php
+++ application/views/editProfil.php
@@ -1,11 +1,11 @@
-<div class="indent2">
+<div id="connexion">
 	<form id="edit-profile-form" method="post" action="/WeShare/Profil/Edit/ModifierProfil/">
-		<h3>Modifier votre profil</h3>	
+		<h1>Modifier votre profil</h1>	
 		<fieldset>
-			<div class="field"><label>Nom :</label><input type="text" name="lastName" id="lastName" value="<?php echo $profil['LastName'];?>"/></div><br />
-			<div class="field"><label>Prénom :</label><input type="text" name="firstName" id="firstName" value="<?php echo $profil['FirstName'];?>"/></div><br />
-			<!--<div class="field"><label>Date de Naissace:</label><input type="text" value=""/></div><br />-->
-			<div class="field"><label>Date de Naissance:</label>
+			<p><label>Nom :</label><input type="text" name="lastName" id="lastName" value="<?php echo $profil['LastName'];?>"/></p>
+			<p><label>Prénom :</label><input type="text" name="firstName" id="firstName" value="<?php echo $profil['FirstName'];?>"/></p>
+			<!--<label>Date de Naissace:</label><input type="text" value=""/><br />-->
+			<p><label>Date de Naissance:</label>
 				<select name="day">
 					<?php
 						for($i=1;$i<32;$i++)
@@ -51,16 +51,16 @@
 						}
 					?>
 				</select>
-			</div><br />
-			<div class="field"><label>Adresse Postale :</label><input type="text" name="address" value="<?php echo $profil['Address'];?>"/></div><br />
-			<div class="field"><label>Ville :</label><input type="text"  name="city" value="<?php echo $profil['City'];?>"/></div><br />
-			<div class="field"><label>Pays :</label><input type="text"  name="country" value="<?php echo $profil['Country'];?>"/></div><br /><br />
-			<div class="field"><label>N° de Téléphone :</label><input type="text" name="phoneNumber" value="<?php echo $profil['Phone'];?>"/></div><br />
-			<div class="field"><label>Adresse mail :</label><input type="text" value="<?php echo $profil['Mail'];?>"/></div><br />
-			<div class="field"><label>Mot de Passe :</label><input type="password" name="password" value="<?php echo $profil['Password'];?>"/></div><br />
-			<div class="field"><label>Confirmer mot de Passe :</label><input type="password" name="retypePassword" value="<?php echo $profil['Password'];?>"/></div><br />
-			<div class="field"><label>Votre Avatar :</label><input type="text" name ="avatar" value="<?php echo $profil['Avatar'];?>"/></div><br />
-			<div class="wrapper"><input type="submit" value="Enregistrer les modifications"></div><br /><br /><br /><br /><br />
+			<br /></p>
+			<p><label>Adresse Postale :</label><input type="text" name="address" value="<?php echo $profil['Address'];?>"/></p>
+			<p><label>Ville :</label><input type="text"  name="city" value="<?php echo $profil['City'];?>"/></p>
+			<p><label>Pays :</label><input type="text"  name="country" value="<?php echo $profil['Country'];?>"/></p>
+			<p><label>N° de Téléphone :</label><input type="text" name="phoneNumber" value="<?php echo $profil['Phone'];?>"/></p>
+			<p><label>Adresse mail :</label><input type="text" value="<?php echo $profil['Mail'];?>"/></p>
+			<p><label>Mot de Passe :</label><input type="password" name="password" value="<?php echo $profil['Password'];?>"/></p>
+			<p><label>Confirmer mot de Passe :</label><input type="password" name="retypePassword" value="<?php echo $profil['Password'];?>"/>
+			<p><label>Votre Avatar :</label><input type="text" name ="avatar" value="<?php echo $profil['Avatar'];?>"/><br /><br />
+			<input type="submit" value="Enregistrer"><br /><br />
 			<a href='/weshare/profil/supprimer/'>Supprimer mon compte</a>
 		</fieldset>
 	</form>
